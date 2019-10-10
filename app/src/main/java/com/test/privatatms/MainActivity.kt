58f0ab5bd1ee3427966e4078b4ee9efaac19ebd2
Radik Kushch <radikushch@gmail.com>
2019-10-10 14:04:21 +0300

    feat: add main activity

---
+++ app/src/main/java/com/test/privatatms/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.test.privatatms
+
+import android.os.Bundle
+import dagger.android.support.DaggerAppCompatActivity
+
+class MainActivity : DaggerAppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
