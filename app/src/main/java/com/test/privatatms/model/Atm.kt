3becd8cdce28408fdb9800d6707d68c24f4239c5
Radik Kushch <radikushch@gmail.com>
2019-10-10 16:41:27 +0300

    feat: add toolbar shadow

--- app/src/main/java/com/test/privatatms/model/Atm.kt
+++ app/src/main/java/com/test/privatatms/model/Atm.kt
@@ -3,6 +3,7 @@ package com.test.privatatms.model
 data class Atm(
     val cityRU: String,
     val fullAddressRu: String,
+    val placeRU: String,
     val latitude: Double,
     val longitude: Double,
     val tw: WorkSchedule
