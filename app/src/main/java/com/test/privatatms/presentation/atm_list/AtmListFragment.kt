009559eadf2e00875a5a410479f4856647910905
Radik Kushch <radikushch@gmail.com>
2019-10-11 15:14:06 +0300

    design: customize searchView

--- app/src/main/java/com/test/privatatms/presentation/atm_list/AtmListFragment.kt
+++ app/src/main/java/com/test/privatatms/presentation/atm_list/AtmListFragment.kt
@@ -5,16 +5,12 @@ import android.view.View
 import com.test.privatatms.R
 import com.test.privatatms.presentation.base.BaseFragment
 import javax.inject.Inject
-import android.R.id
-import android.graphics.Color
 import android.util.Log
-import android.widget.ImageView
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import com.test.privatatms.extensions.invisible
 import com.test.privatatms.extensions.visible
 import kotlinx.android.synthetic.main.fragment_atm_list.*
-
 
 class AtmListFragment : BaseFragment(), AtmListContract.AtmListView {
 
@@ -36,7 +32,6 @@ class AtmListFragment : BaseFragment(), AtmListContract.AtmListView {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupViews()
         showLoading()
         atmListPresenter.getAtmList("Киев").observe(this , Observer {
             if(it.isSuccess) {
@@ -45,12 +40,5 @@ class AtmListFragment : BaseFragment(), AtmListContract.AtmListView {
                 Toast.makeText(requireContext(), "Error", Toast.LENGTH_LONG).show()
             }
         })
-    }
-
-    private fun setupViews() {
-        val searchIcon = atmSearchView.findViewById<ImageView>(androidx.appcompat.R.id.search_button)
-        searchIcon.setColorFilter(Color.WHITE)
-        val closeIcon = atmSearchView.findViewById<ImageView>(androidx.appcompat.R.id.search_close_btn)
-        closeIcon.setColorFilter(Color.WHITE)
     }
 }
